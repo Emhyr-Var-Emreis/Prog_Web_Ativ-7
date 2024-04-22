cdfeec3cffa4cf6e796a5a77be88c84bb2f86551
Emhyr-Var-Emreis <116413878+Emhyr-Var-Emreis@users.noreply.github.com>
2024-04-22 14:40:33 -0300

    Add files via upload

---
+++ Prog_Web_Ativ-7/src/pages/sobre/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+
+export default function Sobre(){
+    return(
+    <Layout>
+    <h1>Sobre</h1>
+    <p>O +Tarefas é um projeto desenvolvido por Wesley Thagore durante as aulas de Programação Web afim de facilitar as rotinas do dia-a-dia cadastrando e listando suas principais tarefas </p>
+    </Layout>
+    )
+}
